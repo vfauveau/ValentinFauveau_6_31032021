aeca0e343753c2085c70097f34330bcdd1da110d
vfauveau <76257448+vfauveau@users.noreply.github.com>
2021-04-27 16:14:15 +0200

    Affichage figcaption / creation de la lightbox
    creation de la lightbox et de son contenu au clic, il reste à mettre les évènements sur les flèches.
    
    remplissage des captions des medias.

--- artist.js
+++ artist.js
@@ -1,29 +1,40 @@
+// header du photographe
+let nameTitle = document.querySelector(".photographer-name");
+let endroit = document.querySelector(".photographer-location");
+let tagline = document.querySelector(".photographer-tagline");
+let imageProfil = document.querySelector('.thumb-photographer-picture');
+let tagWrapper = document.querySelector('.tagWrapper');
+let mediaContainer = document.getElementById('mediaContainer');
+let select = document.getElementById('select');
+let getId = localStorage.getItem("idphot");
+// lightbox
+const lightbox = document.querySelector('.lightbox');
+const closeLightbox = document.querySelector('.lightbox-close');
+const lightboxMedia = document.getElementById('lightbox-content');
+const prevButton = document.querySelector('.lightbox-prev');
+const nextButton = document.querySelector('.lightbox-next');
+// bottom-right-corner text-box
+let spanPrice = document.getElementById('total-photographer-price');
+let spanLike = document.getElementById('total-likes');
 //formulaire DOM
 let modal = document.getElementById('modalWrapper');
 let contact = document.getElementById('contactMe');
 let submit = document.querySelector('.submit-button');
 let closeForm = document.getElementById('closeForm');
-// form inputs
+let contactForm = document.querySelector('.contactForm');
+let inputFirstName = document.getElementById('firstName');
+let inputLastName = document.getElementById('lastName');
+let inputEmail = document.getElementById('email');
+let inputYourMessage = document.getElementById('yourMessage');
 let modalArtistName = document.querySelector('.photographer-nameModal');
-let nameTitle = document.querySelector(".photographer-name");
-let endroit = document.querySelector(".photographer-location");
-let tagline = document.querySelector(".photographer-tagline");
-let imageProfil = document.querySelector('.thumb-photographer-picture');
-let spanPrice = document.getElementById('total-photographer-price');
-let spanLike = document.getElementById('total-likes');
-let tagWrapper = document.querySelector('.tagWrapper');
-let getId = localStorage.getItem("idphot");
-let mediaContainer = document.getElementById('mediaContainer');
-let select = document.getElementById('select');
 
-const lightbox = document.getElementById('lightbox');
-const closeLightbox = document.getElementById('closeLightbox');
-const showMedia = document.querySelectorAll('.thumb-photographie');
-
-// ouverture et fermeture du formulaire
+// ouverture et fermeture du formulaire / de la lightbox
 contact.onclick = () => modalWrapper.style.display = "block";
 closeForm.onclick = () => modalWrapper.style.display = "none";
-closeLightbox.onclick = () => lightbox.style.display = "none";
+closeLightbox.onclick = () => lightbox.style.display= "none";
+
+// envoi du formulaire
+contactForm.onsubmit = () => console.log(inputFirstName.value, inputLastName.value, inputEmail.value, inputYourMessage.value)
 
 class factory {
     static create(type = "image") {
@@ -38,13 +49,36 @@ class imageFactory {
         this._caption = document.createElement('figcaption');
         this._el = document.createElement('img');
         this._el.classList.add('thumb-photographie');
-        this._el.onclick = () => { lightbox.style.display = "block" }
-    }
+        this._el.onclick = () => {
+            lightbox.style.display = "flex";
+            let contenu = document.createElement('img') ;
+            contenu.src = this._el.src ;
+            contenu.classList.add('active');
+            while (lightboxMedia.firstChild) {
+                lightboxMedia.removeChild(lightboxMedia.firstChild)
+                }
+            lightboxMedia.appendChild(contenu);
+            }
+        }
     affich(content) {
         this._el.src = content;
+        let captionTitle = content.replace("Sample Photos/", " ");
+        captionTitle = captionTitle.replace(".jpg", " ");
+        captionTitle = captionTitle.replaceAll("_", " ");
+        this._caption.textContent = captionTitle;
         this._fig.appendChild(this._el);
         this._fig.appendChild(this._caption);
         mediaContainer.appendChild(this._fig);
+    }
+    showPrice (price){
+        let priceText = document.createElement('strong');
+        priceText.textContent=price+" €"
+        this._caption.appendChild(priceText);
+    }
+    showLikes (likes){
+        let likesText =document.createElement('strong');
+        likesText.textContent=likes;
+        this._caption.appendChild(likesText)
     }
 }
 
@@ -53,22 +87,41 @@ class videoFactory {
         this._fig = document.createElement('figure');
         this._caption = document.createElement('figcaption');
         this._el = document.createElement("video");
-        this._el.classList.add('thumb-video')
+        this._el.classList.add('thumb-photographie');
         this._el.appendChild(document.createElement("source"));
-    }
-    getEl() {
-        return this._el;
-    }
+        this._el.onclick = () => {
+            lightbox.style.display = "flex";
+            let contenu = document.createElement('video') ;
+            contenu.src = this._el.src ;
+            contenu.classList.add('active');
+            while (lightboxMedia.firstChild) {
+                lightboxMedia.removeChild(lightboxMedia.firstChild)
+                }
+            lightboxMedia.appendChild(contenu);
+            contenu.play();
+            }
+        }
     affich(content) {
+        let captionTitle = content.replace("Sample Photos/", " ");
+        captionTitle = captionTitle.replaceAll("_", " ");
+        captionTitle = captionTitle.replace(".mp4", " ");
         this._fig.appendChild(this._el);
         this._fig.appendChild(this._caption);
         this._el.children[0].src = content;
         this._el.src = content;
         mediaContainer.appendChild(this._fig);
     }
+    showPrice (price){
+        let priceText = document.createElement('strong');
+        priceText.textContent=price+" €"
+        this._caption.appendChild(priceText);
+    }
+    showLikes (likes){
+        let likesText =document.createElement('strong');
+        likesText.textContent=likes;
+        this._caption.appendChild(likesText)
+    }
 }
-let vid1 = factory.create("video");
-let im1 = factory.create("image");
 
 const myRequest = new Request('data.json')
 fetch(myRequest)
@@ -111,34 +164,37 @@ fetch(myRequest)
         });
         filterDefault.sort(function (a, b) { return b.likes - a.likes });
         /* fonction qui vérifie les éléments contenus dans les tableaux et les transmets à la factory pour qu'elle créé des éléments correspondants au type */
-        function printing(array) {
+        function factorySender(array) {
             array.forEach(element => {
                 if (element.image === undefined) {
                     let vid1 = factory.create("video");
-                    vid1.affich("Sample Photos/" + element.video)
+                    vid1.affich("Sample Photos/" + element.video);
+                    vid1.showPrice(element.price);
+                    vid1.showLikes(element.likes);
                 }
                 else {
                     let im1 = factory.create("image");
-                    im1.affich("Sample Photos/" + element.image)
+                    im1.affich("Sample Photos/" + element.image);
+                    im1.showPrice(element.price);
+                    im1.showLikes(element.likes);
                 }
             });
         }
         // impression de la page défaut (= par Popularité)
-
-        printing(filterDefault);
+        factorySender(filterDefault);
 
         // Détection de changement de selection et affichage en fonction
         select.onchange = () => {
             mediaContainer.innerHTML = "";
             if (select.value === "Popularité") {
-                printing(filterDefault)
+                factorySender(filterDefault)
 
             }
             if (select.value === "Date") {
                 var filterByDate = filteredMedias.sort(function (a, b) {
                     return new Date(b.date) - new Date(a.date);
                 });
-                printing(filterByDate)
+                factorySender(filterByDate)
             }
             if (select.value === "Titre") {
 
@@ -155,3 +211,4 @@ fetch(myRequest)
         console.error('erreur');
         console.error(error);
     });
+
